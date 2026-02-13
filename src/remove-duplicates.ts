ff8159e7811d09403d62487d9a35c100831ca896
krishna123-png <khirani787@gmail.com>
2026-02-13 18:52:24 +0530

    updated the remove-duplicates.ts file

--- src/remove-duplicates.ts
+++ src/remove-duplicates.ts
@@ -5,7 +5,7 @@ function removeDuplicates(nums: number[]): number {
 
     for (let i = 1; i < nums.length; i++) {
         if (nums[i] !== nums[k - 1]) {
-            nums[k] = nums[i];
+            nums[k] = nums[i]!;
             k++;
         }
     }
